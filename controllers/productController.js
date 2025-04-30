464fc7b793607ec1695fdaf64fc6033aa2ae0c84
tahasin0 <158696545+tahasin0@users.noreply.github.com>
2025-05-01 00:00:33 +0600

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,1 +1,9 @@
+const getProducts = (req, res) => {
+  res.json([
+    { id: '1', name: 'Product 1', price: 10 },
+    { id: '2', name: 'Product 2', price: 20 },
+    { id: '3', name: 'Product 3', price: 30 },
+  ]);
+};
 
+module.exports = { getProducts };
